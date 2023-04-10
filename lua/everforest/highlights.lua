f0062dbf35dcfd6155cd5622e027490e68e17158
William Mathewson <wncmathewson@me.com>
2023-04-10 13:17:45 +0100

    Add semantic token highlights

--- lua/everforest/highlights.lua
+++ lua/everforest/highlights.lua
@@ -495,6 +495,22 @@ highlights.generate_syntax = function(palette, options)
     ["@variable"] = { link = "TSVariable" },
     ["@variable.builtin"] = { link = "TSVariableBuiltin" },
 
+    -- LSP Semantic token highlights
+    ["@lsp.type.comment"] = { link = "@comment" },
+    ["@lsp.type.enum"] = { link = "@type" },
+    ["@lsp.type.interface"] = { link = "Identifier" },
+    ["@lsp.type.keyword"] = { link = "@keyword" },
+    ["@lsp.type.namespace"] = { link = "@namespace" },
+    ["@lsp.type.parameter"] = { link = "@parameter" },
+    ["@lsp.type.property"] = { link = "@property" },
+    ["@lsp.type.variable"] = {}, -- use treesitter styles for regular variables
+    ["@lsp.typemod.method.defaultLibrary"] = { link = "@function.builtin" },
+    ["@lsp.typemod.function.defaultLibrary"] = { link = "@function.builtin" },
+    ["@lsp.typemod.operator.injected"] = { link = "@operator" },
+    ["@lsp.typemod.string.injected"] = { link = "@string" },
+    ["@lsp.typemod.variable.defaultLibrary"] = { link = "@variable.builtin" },
+    ["@lsp.typemod.variable.injected"] = { link = "@variable" },
+
     -- p00f/ts-rainbow
     rainbowcol1 = syntax_entry(palette.red, palette.none),
     rainbowcol2 = syntax_entry(palette.orange, palette.none),
