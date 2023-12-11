07e7c211bcc282e5ab6dadcd395c55972fa12d11
William Mathewson <wncmathewson@me.com>
2023-12-11 16:49:19 +0000

    Add types to util module

--- lua/everforest/util.lua
+++ lua/everforest/util.lua
@@ -1,5 +1,7 @@
 local util = {}
 
+---@param group string
+---@param hl Highlight
 util.generate_highlight = function(group, hl)
   -- We can't add a highlight without a group
   if not group then
@@ -9,12 +11,14 @@ util.generate_highlight = function(group, hl)
   vim.api.nvim_set_hl(0, group, hl)
 end
 
+---@param syntax_entries Highlights
 util.generate_highlights = function(syntax_entries)
   for group, highlights in pairs(syntax_entries) do
     util.generate_highlight(group, highlights)
   end
 end
 
+---@param generated_syntax Highlights
 util.load = function(generated_syntax)
   if vim.g.colors_name then
     vim.cmd([[highlight clear]])
